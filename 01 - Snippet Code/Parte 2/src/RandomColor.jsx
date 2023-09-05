ea7065495362f70372c6135dac68c4379edfd6ef
AngelBlackBlue <angel.blackbkue@hotmail.com>
2023-09-04 21:59:04 -0300

    01-parte2Termiado

--- 01 - Snippet Code/Parte 2/src/RandomColor.jsx
+++ 01 - Snippet Code/Parte 2/src/RandomColor.jsx
@@ -1,2 +1,24 @@
 // AQUÍ VA TU COMPONENTE
 // ¡No olvides exportarlo!
+
+import React, { useState } from 'react';
+import './RandomColor.css';
+
+function RandomColor() {
+    const [bgColor, setBgColor] = useState('#ffffff');
+
+    const getRandomColor = () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        setBgColor(randomColor);
+    };
+
+    return (
+        <div className="random-color-container" style={{ backgroundColor: bgColor }}>
+            <div className="yellow-square">
+                <button onClick={getRandomColor}>Cambiar Color</button>
+            </div>
+        </div>
+    );
+}
+
+export default RandomColor;
